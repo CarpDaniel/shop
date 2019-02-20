62dc60bf2d7c587799a06eba173d6409afeb21e5
CarpDaniel <cdchild2005@gmail.com>
2019-02-20 12:45:14 +0200

    CRUD Tests for Location

--- src/test/java/ro/msg/learning/shop/LocationTests.java
+++ src/test/java/ro/msg/learning/shop/LocationTests.java
@@ -11,6 +11,8 @@ import ro.msg.learning.shop.entity.Address;
 import ro.msg.learning.shop.entity.Location;
 import ro.msg.learning.shop.repository.LocationRepository;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(locations="classpath:application-test.properties")
@@ -30,6 +32,43 @@ public class LocationTests  {
         Assert.assertEquals(location.getAddress(), res.getAddress());
         Assert.assertEquals(res.getAddress().getCity(), "Cluj-Napoca");
     }
+
+    @Test
+    public void testReadLocation() {
+        Location location = new Location();
+        location.setName("Test Location");
+        location.setAddress(createTestAddress());
+
+        locationRepository.save(location);
+        Assert.assertEquals(1, ((List<Location>)locationRepository.findAll()).size());
+    }
+
+    @Test
+    public void testUpdateLocation() {
+        Location location = new Location();
+        location.setName("Test Location");
+        location.setAddress(createTestAddress());
+
+        Location res = locationRepository.save(location);
+        Assert.assertNotNull(res);
+        res.setName("Updated Location");
+        Assert.assertEquals(locationRepository.save(res).getName(), "Updated Location");
+    }
+
+    @Test
+    public void testDeleteLocation() {
+        Location location = new Location();
+        location.setName("Test Location");
+        location.setAddress(createTestAddress());
+
+        Location res = locationRepository.save(location);
+        Assert.assertNotNull(res);
+
+        locationRepository.delete(res);
+        Assert.assertEquals(0, ((List<Location>)locationRepository.findAll()).size());
+    }
+
+
 
     private Address createTestAddress() {
         Address testAddress = new Address();
